b5e3443b8f2f67384094777e9d4b2910fb567a4d
FATESAIKOU <qzsecftbhhhh@gmail.com>
2022-03-27 16:40:35 +0900

    Adjust upload.js to read rclone.conf for uploading

--- src/upload.js
+++ src/upload.js
@@ -1,6 +1,18 @@
 const { spawn } = require("child_process");
+const fs = require("fs");
 const driveId = process.argv[2];
 const localPath = process.argv[3];
 const remotePath = process.argv[4];
+const rcloneConf = process.argv[5];
 
-spawn("rclone", ["copy", localPath, `${driveId}:${remotePath}`]);
+fs.writeFile("/tmp/rclone.conf", rcloneConf, function (err) {
+    if (err) return console.log(err);
+});
+
+spawn("rclone", [
+    "--config",
+    "/tmp/rclone.conf",
+    "copy",
+    localPath,
+    `${driveId}:${remotePath}`,
+]);
