cc49b68e37cc6b050d8faa5584befda7966b5759
FATESAIKOU <qzsecftbhhhh@gmail.com>
2022-05-11 12:06:54 +0900

    update build.sh for test

--- env/eks/docker_imgs/worker/build.sh
+++ env/eks/docker_imgs/worker/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build -t btdownloader --build-arg SRC_PATH=. -f ./dockerfile ../../../
+docker build -t btdownloader --build-arg SRC_PATH=. -f ./dockerfile ../../../../
