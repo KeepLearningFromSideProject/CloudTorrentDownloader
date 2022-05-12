4f619816f8ab3855cb08dcbcceabb47b283c7343
FATESAIKOU <qzsecftbhhhh@gmail.com>
2022-05-12 22:07:35 +0900

    Remove redundant code

--- src/worker_main.js
+++ src/worker_main.js
@@ -8,28 +8,11 @@ const sqs = new AWS.SQS({ apiVersion: "2012-11-05" });
 
 
 // SQS operations
-const sendTaskMsg = () => {
-    let params = {
-        MessageBody: `{
-            "request": {
-                "uuid": "1",
-                "magnetLink": "magnet:?xt=urn:btih:KQ3OF6HCXO63OIMOYLCKORO57Q3S3ZXO&dn=&tr=http%3A%2F%2F104.143.10.186%3A8000%2Fannounce&tr=udp%3A%2F%2F104.143.10.186%3A8000%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker3.itzmx.com%3A6961%2Fannounce&tr=http%3A%2F%2Ftracker4.itzmx.com%3A2710%2Fannounce&tr=http%3A%2F%2Ftracker.publicbt.com%3A80%2Fannounce&tr=http%3A%2F%2Ftracker.prq.to%2Fannounce&tr=http%3A%2F%2Fopen.acgtracker.com%3A1096%2Fannounce&tr=https%3A%2F%2Ft-115.rhcloud.com%2Fonly_for_ylbud&tr=http%3A%2F%2Ftracker1.itzmx.com%3A8080%2Fannounce&tr=http%3A%2F%2Ftracker2.itzmx.com%3A6961%2Fannounce&tr=udp%3A%2F%2Ftracker1.itzmx.com%3A8080%2Fannounce&tr=udp%3A%2F%2Ftracker2.itzmx.com%3A6961%2Fannounce&tr=udp%3A%2F%2Ftracker3.itzmx.com%3A6961%2Fannounce&tr=udp%3A%2F%2Ftracker4.itzmx.com%3A2710%2Fannounce&tr=http%3A%2F%2Ftr.bangumi.moe%3A6969%2Fannounce&tr=http%3A%2F%2Ft.nyaatracker.com%2Fannounce&tr=http%3A%2F%2Fopen.nyaatorrents.info%3A6544%2Fannounce&tr=http%3A%2F%2Ft2.popgo.org%3A7456%2Fannonce&tr=http%3A%2F%2Fshare.camoe.cn%3A8080%2Fannounce&tr=http%3A%2F%2Fopentracker.acgnx.se%2Fannounce&tr=http%3A%2F%2Ftracker.acgnx.se%2Fannounce&tr=http%3A%2F%2Fnyaa.tracker.wf%3A7777%2Fannounce&tr=https%3A%2F%2Ftr.bangumi.moe%3A9696%2Fannounce&tr=http%3A%2F%2Ft.acg.rip%3A6699%2Fannounce"
-            },
-            "storage": {
-                "rcloneConf": "conf",
-                "uploadDest": "self:/"
-            }
-        }`,
-        QueueUrl: process.env.SQS_URL,
-    };
-    return sqs.sendMessage(params).promise();
-};
-
 const getTaskMsg = () => {
     let params = {
         QueueUrl: process.env.SQS_URL,
         MaxNumberOfMessages: 1,
-        VisibilityTimeout: 1, // TODO: need to be changed before push
+        VisibilityTimeout: 3600,
     };
     return sqs.receiveMessage(params).promise();
 }
@@ -83,7 +66,6 @@ const parseTaskMsg = (taskMsg) => {
 const main = async () => {
     // get task
     console.log("[Init]");
-    await sendTaskMsg();
     let taskMsg = (await getTaskMsg()).Messages[0];
     let task = parseTaskMsg(taskMsg);
     let request = task.request;
